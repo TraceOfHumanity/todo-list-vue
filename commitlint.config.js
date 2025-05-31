a57862531f2d75ee5a04423a453f0830b59aa79e
Alexander_Ovchar <azalexxx02@gmail.com>
2025-05-31 16:24:21 +0300

    refactor(commitlint): update case rules for type, scope, and subject

--- commitlint.config.js
+++ commitlint.config.js
@@ -17,11 +17,11 @@ export default {
         'chore'    // Інші зміни, які не стосуються src або тестів
       ]
     ],
-    'type-case': [2, 'always', 'lower'],
+    'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
     'scope-empty': [2, 'never'],
-    'scope-case': [2, 'always', 'lower'],
-    'subject-case': [2, 'always', 'lower'],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'always', 'lower-case'],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'header-max-length': [2, 'always', 72]
